097662cd8b186c6da253e35bc33e6ceca4401c77
i3oot <tobias.waller@googlemail.com>
2014-08-03 22:08:26 +0200

    Rename

---
+++ src/main/buttonListener.py
@@ -0,0 +1,9 @@
+import Adafruit_BBIO.GPIO as GPIO
+from moonbowPlayer import play
+
+buttonPin = "P8_19"
+GPIO.setup(buttonPin, GPIO.IN)
+
+while(True):
+	GPIO.wait_for_edge(buttonPin, GPIO.RISING)
+	play()
